2798ec9ced7569ee9ac70fca6706f46de7b9f6fb
Karabi Choudhury <Karabi.Choudhury@ssp-worldwide.com>
2017-11-13 11:51:22 +0530

    populating recipe item

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+
+import {Recipe} from '../../recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent implements OnInit {
-
+  
+  @Input() recipe:Recipe;
   constructor() { }
 
   ngOnInit() {
